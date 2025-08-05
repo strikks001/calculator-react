23417646b30f47ba542d51424367f53c99244b3e
Sanne Strikkers <sanne.strikkers@gmail.com>
2025-08-05 13:08:22 +0200

    making the calculator even more responsive

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,14 +1,26 @@
 import styles from './Display.module.css';
+import { useEffect, useRef } from 'react';
 
 type DisplayProps = {
 	calculation: string;
 };
 
 export default function Display({ calculation }: DisplayProps) {
+	const displayRef = useRef<HTMLDivElement>(null);
+
+	// Ensure the display is always scrolled to the end
+	useEffect(() => {
+		if (displayRef.current) {
+			displayRef.current.scrollLeft = displayRef.current.scrollWidth;
+		}
+	}, [calculation]);
+
 	return (
 		<div className={styles.display}>
 			<div className={styles.display__container}>
-				<div className={styles.display__result}>
+				<div
+					className={styles.display__result}
+					ref={displayRef}>
 					{calculation && (
 						<span className={styles['display--active']}>{calculation}</span>
 					)}
